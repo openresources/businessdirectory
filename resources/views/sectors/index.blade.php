9bb091c89bd04284d22c3be159bac322e2bc5da2
Eviano Afiemo <eviano@cleverpixels.net>
2020-04-07 22:24:10 +0100

    feat: display a proper sector list

--- resources/views/sectors/index.blade.php
+++ resources/views/sectors/index.blade.php
@@ -1,5 +1,23 @@
-@extends('layouts.app') 
+@extends('layouts.default_tpl')
 
 @section('content')
-{{ $sectors }}
+
+<div class="flex items-center justify-between">
+    <h2 class="text-2xl mb-4">{{ __('Sectors') }}</h2>
+</div>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    @foreach ($sectors as $sector)
+
+    <a href="{{ route('sectors.show', $sector) }}">
+        <div
+            class="transition-all flex flex-col items-center justify-center bg-white shadow hover:shadow-lg rounded h-56 p-6">
+            <img src="{{filled($sector->icon)? asset('img/icons/' . $sector->icon) : asset('img/icons/real-estate.svg')}}"
+                alt="icon" class="fill-current text-green-600 hover:opacity-75 hover:skew-y-6  h-20 w-20">
+            <h3 class="text-lg font-semibold text-blue-700 sm:hover:text-pink-700 mt-4"> {{ "$sector->name ($sector->businesses_count)"}} </h3>
+        </div>
+    </a>
+
+    @endforeach
+</div>
 @endsection
