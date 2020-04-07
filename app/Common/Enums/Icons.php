4010e49cafdf9f27ea1829ffb8ac205bf1178925
Eviano Afiemo <eviano@cleverpixels.net>
2020-04-07 16:00:10 +0100

    feat: add sector icon to database seed

---
+++ app/Common/Enums/Icons.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Common\Enums;
+
+use App\Common\Enums\Enum;
+
+class Icons extends Enum
+{
+
+    const BUSINESS_SERVICES = 'money.svg';
+    const COMMUNICATION_SERVICES = 'smart-city.svg';
+    const CONSUMER_PRODUCTS = 'products.svg';
+    const CONSUMER_SERVICES = 'service.svg';
+    const ENERGY = 'energy.svg';
+    const FINANCIALS = 'bank.svg';
+    const FOOD_BEVERAGES = 'food.svg';
+    const HEALTH_CARE = 'health.svg';
+    const INDUSTRIAL = 'industry.svg';
+    const INFORMATION_TECHNOLOGY = 'gps.svg';
+    const MATERIALS = 'diamond.svg';
+    const REAL_ESTATE = 'real-estate';
+    const TECHNOLOGY_COMMUNICATIONS = 'satellite.svg';
+    const UTILITIES = 'gear.svg';
+
+}
--- database/seeds/SectorsTableSeeder.php
+++ database/seeds/SectorsTableSeeder.php
@@ -1,16 +1,27 @@
 <?php
 
+use App\Common\Enums\Icons;
 use App\Common\Enums\Sectors;
 use App\Sector;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class SectorsTableSeeder extends Seeder
 {
     public function run()
     {
+        $icons = Icons::toArray();
+
         foreach (Sectors::getValues() as $value) {
             $name = (String) Str::of(Sectors::getDescription($value))->title();
-            factory(App\Sector::class)->create(['name' => $name, 'description' => $name]);
+
+            $key = Sectors::getKey($value);
+
+            factory(App\Sector::class)->create([
+                'name' => $name,
+                'description' => $name,
+                'icon' => $icons[$key]
+            ]);
         }
     }
 }
