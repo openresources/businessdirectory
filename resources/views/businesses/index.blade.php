a66dd55c9b2b5525063a59d4568fbf7915dadabd
Eviano Afiemo <eviano@cleverpixels.net>
2020-04-09 00:55:27 +0100

    style: change layout template

--- resources/views/businesses/index.blade.php
+++ resources/views/businesses/index.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.default_tpl')
 
 @section('content')
     <h2>{{ __('Directory Listing Title') }}</h2>
 
     @foreach ($businesses as $business)
-           <div>
+           <div class="card w-full md:w-1/2 lg:w-2/3 mb-4">
                {{ $business->name }}
            </div>
     @endforeach
