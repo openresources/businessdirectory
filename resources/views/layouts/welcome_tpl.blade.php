9de0cbb5895a66925dbb87c0b6601af6bb08c0bf
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-04-19 23:13:53 +0100

    style: change header layout

--- resources/views/layouts/welcome_tpl.blade.php
+++ resources/views/layouts/welcome_tpl.blade.php
@@ -3,6 +3,12 @@
 @section('scaffold')
 <div class="w-full">
     <div class="flex bg-blue-900" style="height:500px;">
+        <div class="hidden lg:block lg:w-1/2" style="clip-path: polygon(0 0, 100% 0%, 75% 100%, 0% 100%);">
+            <div class="h-full object-cover"
+                style="background-image: url(https://images.unsplash.com/photo-1444653389962-8149286c578a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1645&q=80)">
+                <div class="h-full bg-black opacity-25"></div>
+            </div>
+        </div>
         <div class="flex items-center text-center lg:text-left px-20 lg:w-1/2">
             <div>
                 <h2 class="text-3xl font-semibold text-gray-100 md:text-4xl">Your search starts<span
@@ -20,12 +26,6 @@
                         </button>
                     </form>
                 </div>
-            </div>
-        </div>
-        <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
-            <div class="h-full object-cover"
-                style="background-image: url(https://images.unsplash.com/photo-1444653389962-8149286c578a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1645&q=80)">
-                <div class="h-full bg-black opacity-25"></div>
             </div>
         </div>
     </div>
