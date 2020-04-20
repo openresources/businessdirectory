5aea1c409c515781448152d3de8669f8ddb82028
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-04-20 02:38:12 +0100

    style: add tabbed navigation

--- resources/views/layouts/default_tpl.blade.php
+++ resources/views/layouts/default_tpl.blade.php
@@ -6,29 +6,39 @@
         <div class="container mx-auto">
             <form action="{{ route('search.index') }}" method="GET">
                 <input class="w-full h-10 px-3 rounded focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg"
-                    name="keyword" type="search" placeholder="Enter search key (e.g. company name, town, service type or sector)">
+                    name="keyword" type="search"
+                    placeholder="Enter search key (e.g. company name, town, service type or sector)">
             </form>
         </div>
     </section>
-    
+
     <section class="bg-gray-300">
+        @php
+            use Illuminate\Support\Str;
+            $highlightAddBusinessTab = Str::of(url()->current())->contains(route('businesses.create')) ;
+            $highlightFindBusinessTab = ! Str::of(url()->current())->contains(route('businesses.create'));
+            $activeTabClass  = "bg-white py-3";
+            $inactiveTabClass = "pt-3 pb-2";
+        @endphp
+
         <div class="container mx-auto pt-16">
             <ul class="flex border-b">
-                <li class="-mb-px mr-1">
-                    <a class="bg-white inline-block border-l border-t border-r rounded-t py-3 px-8 text-xl text-gray-700 no-underline hover:underline font-semibold"
-                        href="#">Find a Business</a>
+                <li class="{{ $highlightFindBusinessTab? '-mb-px' : ''}} mr-1">
+                    <a class="{{$highlightFindBusinessTab? $activeTabClass :  $inactiveTabClass }} inline-block border-l border-t border-r rounded-t px-8 text-xl text-gray-700 no-underline hover:underline font-semibold"
+                href="{{route('sectors.index')}}">Find a Business</a>
                 </li>
-                <li class="mr-1">
-                    <a class="inline-block border-l border-t border-r rounded-t py-2 px-8 text-xl text-gray-700 no-underline hover:underline font-semibold" href="#">Add your Business</a>
+                <li class="{{ $highlightAddBusinessTab? '-mb-px' : ''}} mr-1">
+                    <a class="{{$highlightAddBusinessTab? $activeTabClass :  $inactiveTabClass }} inline-block border-l border-t border-r rounded-t px-8 text-xl text-gray-700 no-underline hover:underline font-semibold"
+                        href="{{ route('businesses.create') }}">Add your Business</a>
                 </li>
             </ul>
         </div>
     </section>
 
 
-        <div class="container mx-auto py-16">
-            @yield('content')
-        </div>
+    <div class="container mx-auto py-16">
+        @yield('content')
+    </div>
 
 </div>
 @endsection
