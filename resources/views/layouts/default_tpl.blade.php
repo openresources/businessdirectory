4a6e4ce3d3361092589f7d27c6481f6f5cde3e05
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-04-20 01:35:34 +0100

    feat: enable global search functionality

--- resources/views/layouts/default_tpl.blade.php
+++ resources/views/layouts/default_tpl.blade.php
@@ -4,7 +4,7 @@
 <div class="min-h-screen">
     <section class="bg-indigo-900 py-6">
         <div class="container mx-auto">
-            <form action="" method="GET">
+            <form action="{{ route('search.index') }}" method="GET">
                 <input class="w-full h-10 px-3 rounded focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg"
                     name="keyword" type="search" placeholder="Enter search key (e.g. company name, town, service type or sector)">
             </form>
